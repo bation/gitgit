28e318d5a46fb56d4fa13842decd9326c3b2158e
bation <854585638@qq.com>
2015-06-14 13:49:26 +0800

    add a line

--- helloWorld.java
+++ helloWorld.java
@@ -3,5 +3,6 @@
 public class helloWorld{
 	public static void main(String [] args){
 		System.out.println("helloWorld! this is my first code for git")
+		System.out.println("now check my new words")
 	}
 }
